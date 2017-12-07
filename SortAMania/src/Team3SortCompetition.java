6d085c733f1ea1d7379d4a025e5a1001ae7f6959
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-12-07 13:10:06 -0500

    finished5

--- SortAMania/src/Team3SortCompetition.java
+++ SortAMania/src/Team3SortCompetition.java
@@ -91,10 +91,71 @@ public abstract class Team3SortCompetition extends SortCompetition{
 				{
 					swap(medval,j,j-1);
 				}
+			
 			}
 		}
 		return getmedian(medval);
 	}
+	public int challengeFive(Comparable[] arr, Comparable query)
+	{
+		mergeSort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==query)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public Comparable[] mergecomparable(Comparable[] list1, Comparable[] list2)
+	{
+		int newlist = 0;
+		int newlist2 = 0;
+		int newlist3 = 0;
+		Comparable[] newarray = new Comparable[list1.length + list2.length];
+		while(newlist < list1.length || newlist2 < list2.length)
+		{
+			if (newlist == list1.length)
+			{
+				newarray[newlist3] = list2[newlist2];
+				newlist2++;
+			}
+			else if (newlist2 == list2.length)
+			{
+				newarray[newlist3] = list1[newlist];
+				newlist++;
+			}
+			else if (list1[newlist].compareTo(list2[newlist2]) < 0)
+			{
+				newarray[newlist3] = list1[newlist];
+				newlist++;
+			}
+			else 
+			{
+				newarray[newlist3] = list2[newlist2];
+				newlist2++;
+			}
+			newlist3++;
+		}
+		return newarray;
+	}
+	public Comparable[] mergeSort(Comparable[] list)
+	{
+		if (list.length <= 1) 
+		{
+			return list;
+		}
+		else
+		{
+			int x=list.length/2;
+			Comparable[] temp1 = Arrays.copyOfRange(list,0,x);
+			Comparable[] temp2 = Arrays.copyOfRange(list,x,list.length);
+			return mergecomparable(mergeSort(temp1),mergeSort(temp2));
+		}
+	}
+
 	public void swap(int[]arr,int i,int j)
 	{
 		int x = arr[i];
